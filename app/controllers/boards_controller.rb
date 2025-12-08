939c9b6383bbcc6588b2a27339d4087a7606e9cc
sherrycocomo <milk_child_baby@yahoo.co.jp>
2025-12-08 11:10:51 +0900

    formを表示

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,5 +2,20 @@ class BoardsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @boards = Board.all
+  end
+
+  def new
+    @board = current_user.boards.build
+  end
+
+  def create
+    @board = current_user.board.build(board_params)
+    if @board.save
+      redirect_to board_path(@board), notice: '保存できたよ'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'boards#index'
+
+  resources :boards, only: [:new, :create]
 end
