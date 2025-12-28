03554aa91b1903dd4be0f16ceec514b4fc268146
sherrycocomo <milk_child_baby@yahoo.co.jp>
2025-12-28 14:46:25 +0900

    taskテーブルにuser_id を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,6 +8,11 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_tasks_on_user_id  (user_id)
 #
 class Task < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_28_051200) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_28_053936) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_28_051200) do
     t.datetime "deadline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
