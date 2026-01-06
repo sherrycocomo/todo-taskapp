034f702f3869166cc43e0f826d03b677dfd96abc
sherrycocomo <milk_child_baby@yahoo.co.jp>
2026-01-06 16:11:00 +0900

    Commentモデルを作成した

---
+++ app/models/comment.rb
@@ -0,0 +1,11 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Comment < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_30_075641) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_06_070350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_30_075641) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
