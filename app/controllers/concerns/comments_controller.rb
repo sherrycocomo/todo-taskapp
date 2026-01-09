da4f31f56f87c379aae6e4e67ee244262b72cbd0
sherrycocomo <milk_child_baby@yahoo.co.jp>
2026-01-09 15:24:59 +0900

    commentコントローラーにcreateアクションを追加

--- app/controllers/concerns/comments_controller.rb
+++ app/controllers/concerns/comments_controller.rb
@@ -7,6 +7,18 @@ class CommentsController < ApplicationController
         @comment = @task.comments.build(user: current_user)
     end
 
+    def create
+        @comment = @task.comments.build(comment_params)
+        @comment.user = current_user
+
+        if @task.save
+            redirect_to board_task_path(@board, @task), notice: '保存できたよ'
+        else
+            flash.now[:error] = '保存に失敗しました'
+            render :new, status: :unprocessable_entity
+        end
+    end
+
     private
     def set_board
         @board = current_user.boards.find(params[:board_id])
@@ -14,5 +26,9 @@ class CommentsController < ApplicationController
 
     def set_task
         @task = @board.tasks.find(params[:task_id])
+    end
+
+    def comment_params
+        params.require(:comment).permit(:content)
     end
 end
