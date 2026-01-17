d3bdd9ea0193ccd9a20cad9c189aaff10aef17fc
sherrycocomo <milk_child_baby@yahoo.co.jp>
2026-01-17 10:39:12 +0900

    updateアクション

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,35 @@
 class ProfilesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_profile, only:[:edit, :update]
 
     def show
         @profile = current_user.profile || current_user.create_profile
     end
 
     def edit
-        @profile = current_user.build_profile
+    end
+
+    def update
+        if @profile.update(profile_params)
+            redirect_to profile_path, notice: 'プロフィール更新！'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+
+    private
+    def profile_params
+        params.require(:profile).permit(
+            :nickname,
+            :introduction,
+            :gender,
+            :birthday,
+            :subscribed
+        )
+    end
+
+    def set_profile
+        @profile = current_user.profile || current_user.build_profile
     end
 end
