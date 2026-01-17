cc982c1152596de45eda3eb7b5a0e9aeea7045b5
sherrycocomo <milk_child_baby@yahoo.co.jp>
2026-01-17 14:18:50 +0900

    年齢を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one :profile, dependent: :destroy
 
-  delegate :birthday, :gender, to: :profile, allow_nil: true
+  delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
   def display_name
     profile&.nickname || self.email.split('@').first
